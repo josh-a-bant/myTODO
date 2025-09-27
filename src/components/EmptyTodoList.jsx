595dfa0256a73db4291a837694a67109959af2a8
josh-a-bant <jashobantbesra@gmail.com>
2025-09-27 23:38:38 +0530

    build a todo list application with functionality

---
+++ src/components/EmptyTodoList.jsx
@@ -0,0 +1,9 @@
+export function EmptyTodoList() {
+  return (
+    <div className="mt-8">
+      <h3 className="text-center text-2xl md:text-3xl font-bold text-violet-950">
+        NO TASK TO DISPLAY
+      </h3>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full bg-violet-900 text-violet-100 px-4 py-2">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        <div className="text-sm md:text-xl">
+          <span className="text-xs md:text-base">my</span>TODO
+        </div>
+        <ul className="flex gap-4 md:gap-6 list-none ">
+          <li className="cursor-pointer text-sm md:text-base hover:scale-110 hover:text-white transform transition-all duration-200">
+            Home
+          </li>
+          <li className="cursor-pointer text-sm md:text-base hover:scale-110 hover:text-white transform transition-all duration-200">
+            Your Task
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
