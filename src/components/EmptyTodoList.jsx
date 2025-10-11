75ff017fa20a0be4f192f7025fc9a22d94370e0a
josh-a-bant <jashobantbesra@gmail.com>
2025-10-12 00:48:53 +0530

    implement redux to manage state

--- src/components/EmptyTodoList.jsx
+++ src/components/EmptyTodoList.jsx
@@ -1,7 +1,7 @@
 export function EmptyTodoList() {
   return (
-    <div className="mt-8">
-      <h3 className="text-center text-2xl md:text-3xl font-bold text-violet-950">
+    <div className="mt-10">
+      <h3 className="text-center text-4xl font-bold text-violet-950">
         NO TASK TO DISPLAY
       </h3>
     </div>
